a3aa16b51f5f20f62447f4d34dfb3eaeca29404a
alex-dev-hk <awong199335@gmail.com>
2022-04-19 22:30:47 +0800

    adding docker-compose

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
   <div>
       <ChartContainer />
-      <div><Link href={"/client"}><a target="_blank" rel="noopener noreferrer">client</a></Link></div>
+      <div style={{position:'absolute',top:'85%',right:'50%',transform: 'translate(80%, 50%)'}}><Link href={"/client"}><a target="_blank" rel="noopener noreferrer">client</a></Link></div>
   </div>
   )}
 
